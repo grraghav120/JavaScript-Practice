09f1445cf5a85acafe426fd9fbd58f6ebcd7c10d
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2022-12-06 21:38:14 +0530

    01

--- Set_In_JS.js
+++ Set_In_JS.js
@@ -1,4 +1,4 @@
-let dist=new Set();
+let dist = new Set();
 
 dist.add(5);
 dist.add(4);
@@ -6,10 +6,10 @@ dist.add(2);
 dist.add(1);
 dist.add(10);
 
-let nums=[1,2,3,4,5,6,7,8,9]
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-nums.forEach((x)=>{
-    dist.add(x);
+nums.forEach((x) => {
+  dist.add(x);
 });
 
 console.log(dist);
