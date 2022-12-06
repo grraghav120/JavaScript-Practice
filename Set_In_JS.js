b817a1e1933b5eee789f829fcb7acd3e651c5c0a
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2022-12-06 07:40:36 +0530

    changes

---
+++ Set_In_JS.js
@@ -0,0 +1,15 @@
+let dist=new Set();
+
+dist.add(5);
+dist.add(4);
+dist.add(2);
+dist.add(1);
+dist.add(10);
+
+let nums=[1,2,3,4,5,6,7,8,9]
+
+nums.forEach((x)=>{
+    dist.add(x);
+});
+
+console.log(dist);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+// dist.add(4);
+// dist.add(2);
+// dist.add(1);
+// dist.add(10);
